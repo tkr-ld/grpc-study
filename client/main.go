d36149cda63ed99b38ac77354e4411fc6e2c1842
tkr-ld <sonobetakuro@team-lab.com>
2023-12-31 11:39:57 +0900

    クライアント側のコード作成

--- client/main.go
+++ client/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	grpc_weather "github.com/tkr-ld/grpc-study/client/gen/api/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
 
 func main() {
-	fmt.Printf("Hello world")
+	conn, err := grpc.Dial(
+		"localhost:50051",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	client := grpc_weather.NewWeatherServiceClient(conn)
+
+	req := &grpc_weather.GetRequest{
+		Number: 1,
+	}
+
+	res, err := client.GetWeather(context.Background(), req)
+	if err != nil {
+		fmt.Printf("error")
+	}
+
+	fmt.Printf("weather: %v\n", res.GetWeather())
 }
